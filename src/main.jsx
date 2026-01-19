770baaf433c3fabd634f53b39fa165839cd183fa
antonioval-sys <antonio.drinkcafe@gmail.com>
2026-01-19 00:18:41 -0600

    Wrap App component with BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import './Style.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter basename="/restaurante-web/">
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 )
