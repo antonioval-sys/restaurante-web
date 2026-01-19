4cfe4181fe6fbe298b82fa58a3d11e1ce6327cf1
antonioval-sys <antonio.drinkcafe@gmail.com>
2026-01-19 01:48:16 -0600

    hgj

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/restaurante-web/',
   plugins: [react()],
   base: '/restaurante-web/',
 })
